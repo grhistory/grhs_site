67cabfd8cd5a75cdba64b13169a938521d9db6bd
CJ Davis <cj@cjdavis.me>
2018-11-04 17:15:26 -0500

    Add more bootstrapping to setup bash script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,3 +11,7 @@ cd "./devops"
 
 docker-compose exec app python manage.py migrate
 docker-compose exec app python manage.py copy_media
+docker-compose exec app python manage.py setup_basic_pages
+docker-compose exec app python manage.py create_baxter_award_pages
+docker-compose exec app python manage.py create_product_pages
+docker-compose exec app sh -c 'echo "from django.contrib.auth.models import User; User.objects.create_superuser(\"superuser\", \"superuser@example.com\", \"pass\")" | ./manage.py shell'
