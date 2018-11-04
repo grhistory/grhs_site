83c4ef0a8523359ad71c3f8e9d1e1ba325a23f09
CJ Davis <cj@cjdavis.me>
2018-11-04 11:13:16 -0500

    Add information around Docker setup

---
+++ scripts/prepare.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+echo 'TODO: Make this work'
+
+# scriptdir="$(dirname "$0")"
+# cd "$scriptdir/.."
+
+# echo 'Checking if app-building images are available...'
+
+# cd "./devops"
+
+# docker-compose exec app python manage.py migrate
+# docker-compose exec app python manage.py copy_media
